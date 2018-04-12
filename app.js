1dd51794ac332ef2c87a7b3605227ada4fb121e0
Moritz Wegner <moritzwegner46@gmail.com>
2018-04-12 19:53:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const third_party = require('./routes/third-party.js');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/lol/champion-mastery/v3/', champion_mastery);
 app.use('/lol/platform/v3/', champion);
 app.use('/lol/league/v3/', league);
